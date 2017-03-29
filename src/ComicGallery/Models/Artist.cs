25774a7a757c1438865816711150629cde51a302
Karol <Karol@KAROL>
2017-03-29 22:27:24 +0200

    Added ComicBook and Artist models

---
+++ src/ComicGallery/Models/Artist.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicGallery.Models {
+    public class Artist {
+        public string Name { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ src/ComicGallery/Models/ComicBook.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicGallery.Models {
+    public class ComicBook {
+        public int Id { get; set; }
+        public string seriesTitle { get; set; }
+        public int IssueNumber { get; set; }
+        public string DescriptionHtml { get; set; }
+        public Artist[] Artists { get; set; }
+        public bool Favourite { get; set; }
+    }
+}
